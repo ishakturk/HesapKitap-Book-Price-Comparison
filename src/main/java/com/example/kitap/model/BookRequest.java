88b751cfdac390f839a5d95fdc35a2a364f008ec
Ishak Turk <ishakturk33@gmail.com>
2025-01-28 16:41:25 +0300

    reformatted

--- src/main/java/com/example/kitap/model/BookRequest.java
+++ src/main/java/com/example/kitap/model/BookRequest.java
@@ -11,17 +11,50 @@ public class BookRequest {
     private String imageUrl;
     private List<BookPriceModel> prices;
 
-    // Getters & Setters
-    public String getIsbn() { return isbn; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-    public String getPublisher() { return publisher; }
-    public void setPublisher(String publisher) { this.publisher = publisher; }
-    public String getImageUrl() { return imageUrl; }
-    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
-    public List<BookPriceModel> getPrices() { return prices; }
-    public void setPrices(List<BookPriceModel> prices) { this.prices = prices; }
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<BookPriceModel> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<BookPriceModel> prices) {
+        this.prices = prices;
+    }
 }
